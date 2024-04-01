d27fcbd8eb3de34d7a644beb582c8b827fdbf1c8
TomasRay <tomasvaiciunas@live.com>
2024-04-01 17:23:38 +0300

    Update

--- app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/MenuActivity.java
+++ app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/MenuActivity.java
@@ -1,5 +1,6 @@
 package edu.vvk_pit_21_i_nt.countyourcalories;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -8,20 +9,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import edu.vvk_pit_21_i_nt.countyourcalories.databinding.ActivityMainBinding;
+import edu.vvk_pit_21_i_nt.countyourcalories.databinding.ActivityMenuBinding;
 
 public class MenuActivity extends AppCompatActivity {
 
+    ActivityMenuBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(Item ->{
 
-        Toast.makeText(MenuActivity.this,  "Įšsaugota", Toast.LENGTH_LONG).show();
+            switch (Item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.health:
+                    replaceFragment(new HealthFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+
+            }
+
+                return true;
+        } );
+
+
+
+
+//
+//         EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_menu);
+//
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//
+//        Toast.makeText(MenuActivity.this,  "Įšsaugota", Toast.LENGTH_LONG).show();
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
