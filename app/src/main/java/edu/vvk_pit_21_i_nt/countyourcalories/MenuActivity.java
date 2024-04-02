001df1be1947fc38c1410bba576898f0febd1e01
Ina-Mar <115873882+Ina-Mar@users.noreply.github.com>
2024-04-02 14:40:24 +0300

    Google sign-in added

--- app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/MenuActivity.java
+++ app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/MenuActivity.java
@@ -13,7 +13,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MenuActivity extends AppCompatActivity {
-    private FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
