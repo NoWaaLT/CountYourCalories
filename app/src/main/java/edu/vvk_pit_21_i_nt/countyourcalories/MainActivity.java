cff644f90cb5df7501e72dd3b51ee26996ccd7a5
Ina-Mar <115873882+Ina-Mar@users.noreply.github.com>
2024-04-06 18:07:31 +0300

    Sign-Up updated

--- app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/MainActivity.java
+++ app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, MenuActivity.class);
             startActivity(intent);
         }
-        //signIn();
+
 
         upperText = findViewById(R.id.textView2);
         spinner = findViewById(R.id.gender_spinner);
