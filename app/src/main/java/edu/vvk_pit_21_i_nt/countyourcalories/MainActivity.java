82d45ac4496b3bc4181f6f2a828320c984b66073
NoWaaLT <vytautas.jelisejevas@gmail.com>
2024-04-14 23:25:12 +0300

    sd

--- app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/MainActivity.java
+++ app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     Button buttonBack;
     TextView upperText;
     TextView progressText;
+    ImageView backArrow;
     EditText inputText;
     Button signoutButton;
     ProgressBar progressBar;
@@ -56,18 +58,16 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//  this      mDatabase = FirebaseDatabase.getInstance().getReference();
-//        user = FirebaseAuth.getInstance().getCurrentUser();
-//
-//
-//        if (user == null) {
-//            signIn();
-//        }
-//        else {
-//            Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-//            startActivity(intent);
-//        }
-//
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user == null) {
+            signIn();
+        }
+        else {
+            Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+            startActivity(intent);
+        }
 
         upperText = findViewById(R.id.textView2);
         spinner = findViewById(R.id.gender_spinner);
@@ -77,155 +77,21 @@ public class MainActivity extends AppCompatActivity {
         signoutButton = findViewById(R.id.signout_button);
         progressText = findViewById(R.id.textViewSteps);
         progressBar = findViewById(R.id.entryProgressBar);
+        backArrow = findViewById(R.id.back_arrow);
 
-// this        signOut();
+        signOut();
         setUpGUI(0, getResources().getStringArray(R.array.gender_list));
 
-        buttonNext.setOnClickListener(v -> {
-            String inputString;
-            switch (preMenuStage) {
-                case 0:
-                    gender = spinner.getSelectedItem().toString();
-                    preMenuStage++;
-                    setUpGUI(1, getResources().getStringArray(R.array.main_goal));
-                    buttonBack.setVisibility(View.VISIBLE);
-                    break;
-                case 1:
-                    String goalString = spinner.getSelectedItem().toString();
-                    switch (goalString) {
-                        case "Gain weight":
-                            goal = 0;
-                            difference = 300;
-                            break;
-                        case "Lose weight":
-                            goal = 1;
-                            difference = -300;
-                            break;
-                        case "Maintain weight":
-                            goal = 2;
-                            difference = 0;
-                            break;
-                        default:
-                            break;
-                    }
-                    preMenuStage++;
-                    setUpGUI(2, getResources().getStringArray(R.array.activity_desc));
-                    break;
-                case 2:
-                    String activityString = spinner.getSelectedItem().toString();
-                    switch (activityString) {
-
-                        case "0-1":
-                            activityLevel = 1.2f;
-                            break;
-                        case "2-3":
-                            activityLevel = 1.375f;
-                            break;
-                        case "4-5":
-                            activityLevel = 1.55f;
-                            break;
-                        case "6-7":
-                            activityLevel = 1.725f;
-                            break;
-                        case "2 k./d.":
-                            activityLevel = 1.9f;
-                            break;
-                        default:
-                            break;
-                    }
-                    preMenuStage++;
-                    setUpGUI(3, getResources().getString(R.string.age));
-                    spinner.setVisibility(View.INVISIBLE);
-                    inputText.setVisibility(View.VISIBLE);
-                    break;
-                case 3:
-                    inputString = inputText.getText().toString();
-                    if(!inputString.isEmpty()) {
-                        age = Integer.parseInt(inputString);
-                        preMenuStage++;
-                        setUpGUI(4, getResources().getString(R.string.weight));
-                    }
-                    else {
-                        Toast.makeText(MainActivity.this, getResources().getString(R.string.emptyField), Toast.LENGTH_SHORT).show();
-                    }
-                    break;
-                case 4:
-                    inputString = inputText.getText().toString();
-                    if(!inputString.isEmpty()) {
-                        weight = Float.parseFloat(inputText.getText().toString());
-                        preMenuStage++;
-                        setUpGUI(5, getResources().getString(R.string.height));
-                    }
-                    else {
-                        Toast.makeText(MainActivity.this, getResources().getString(R.string.emptyField), Toast.LENGTH_SHORT).show();
-                    }
-                    break;
-                case 5:
-                    inputString = inputText.getText().toString();
-                    if(!inputString.isEmpty()) {
-                        height = Integer.parseInt(inputText.getText().toString());
-                        bmr = calcBmr(weight, height, age, gender);
-                        targetKcal = calcTargetKcal(bmr, activityLevel, difference);
-
-// this                        addUserData();
-
-
-                        // Moves to the next activity
-
-//                        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-//                        startActivity(intent);
-//                        finish();
-                    }
-                    else {
-                        Toast.makeText(MainActivity.this, getResources().getString(R.string.emptyField), Toast.LENGTH_SHORT).show();
-                    }
-                    break;
-                default:
-                    break;
-            }
-        });
-
-        buttonBack.setOnClickListener(v -> {
-            if(preMenuStage != 0) {
-                switch(preMenuStage) {
-                    case 1:
-                        preMenuStage--;
-                        setUpGUI(0, getResources().getStringArray(R.array.gender_list));
-                        buttonBack.setVisibility(View.INVISIBLE);
-                        break;
-                    case 2:
-                        preMenuStage--;
-                        setUpGUI(1, getResources().getStringArray(R.array.main_goal));
-                        break;
-                    case 3:
-                        preMenuStage--;
-                        setUpGUI(2, getResources().getStringArray(R.array.activity_desc));
-                        spinner.setVisibility(View.VISIBLE);
-                        inputText.setVisibility(View.INVISIBLE);
-                        break;
-                    case 4:
-                        preMenuStage--;
-                        setUpGUI(3, getResources().getString(R.string.age));
-                        break;
-                    case 5:
-                        preMenuStage--;
-                        setUpGUI(4, getResources().getString(R.string.height));
-                        break;
-                    case 6:
-                        preMenuStage--;
-                        setUpGUI(5, getResources().getString(R.string.weight));
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
+        buttonNext.setOnClickListener(v -> moveNext(v));
+        buttonBack.setOnClickListener(v -> moveBack(v));
+        backArrow.setOnClickListener(v -> moveBack(v));
     }
 
     private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
             new FirebaseAuthUIActivityResultContract(),
             result -> onSignInResult(result)
     );
+
     private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
         IdpResponse response = result.getIdpResponse();
         if (result.getResultCode() == RESULT_OK) {
@@ -236,13 +102,12 @@ public class MainActivity extends AppCompatActivity {
                 mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (!dataSnapshot.hasChild("Users/"+userUid)) {
+                        if (!dataSnapshot.hasChild("Users/" + userUid)) {
 
                             mDatabase.child("Users").child(userUid).child("Display Name").setValue(user.getDisplayName());
                             mDatabase.child("Users").child(userUid).child("Email").setValue(user.getEmail());
                             Log.v("Prisijungimas", userUid);
-                        }
-                        else {
+                        } else {
                             Intent intent = new Intent(MainActivity.this, MenuActivity.class);
                             startActivity(intent);
                         }
@@ -258,8 +123,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             if (response == null) {
                 signIn();
-            }
-            else {
+            } else {
                 Log.e("Prisijungimo klaida", response.getError().getMessage());
             }
             // Sign in failed. If response is null the user canceled the
@@ -268,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
             // ...
         }
     }
+
     void signIn() {
 
         List<AuthUI.IdpConfig> providers = Arrays.asList(
@@ -282,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
         signInLauncher.launch(signInIntent);
 
     }
+
     void signOut() {
         signoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -298,14 +164,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-   private void addUserData() {
-       user = FirebaseAuth.getInstance().getCurrentUser();
-       if (user != null) {
-           String userUid = user.getUid();
-           UserDb userDb = new UserDb(user.getEmail(), user.getDisplayName(), (long) weight, height, activityLevel, age, gender, (long) bmr, goal, targetKcal);
-           mDatabase.child("Users").child(userUid).setValue(userDb);
-       }
-   }
+
+    private void addUserData() {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String userUid = user.getUid();
+            UserDb userDb = new UserDb(user.getEmail(), user.getDisplayName(), (long) weight, height, activityLevel, age, gender, (long) bmr, goal, targetKcal);
+            mDatabase.child("Users").child(userUid).setValue(userDb);
+        }
+    }
 
 
     private void setUpGUI(int questionNum, String[] valuesArr) {
@@ -331,11 +198,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private float calcManBMR(float weight, int height, int age) {
-    return (10f * weight) + (6.25f * height) - (5f * age) + 5f;
+        return (10f * weight) + (6.25f * height) - (5f * age) + 5f;
     }
 
     private float calcWomanBMR(float weight, int height, int age) {
-       return (10f * weight) + (6.25f * height) - (5f * age) - 16;
+        return (10f * weight) + (6.25f * height) - (5f * age) - 16;
     }
 
     private int calcTargetKcal(float bmr, float activityLevel, int difference) {
@@ -347,43 +214,141 @@ public class MainActivity extends AppCompatActivity {
         progressText.setText(stepProgress);
     }
 
+    public void moveNext(View view) {
+        String inputString;
+        switch (preMenuStage) {
+            case 0:
+                gender = spinner.getSelectedItem().toString();
+                preMenuStage++;
+                setUpGUI(1, getResources().getStringArray(R.array.main_goal));
+                buttonBack.setVisibility(View.VISIBLE);
+                break;
+            case 1:
+                String goalString = spinner.getSelectedItem().toString();
+                switch (goalString) {
+                    case "Gain weight":
+                        goal = 0;
+                        difference = 300;
+                        break;
+                    case "Lose weight":
+                        goal = 1;
+                        difference = -300;
+                        break;
+                    case "Maintain weight":
+                        goal = 2;
+                        difference = 0;
+                        break;
+                    default:
+                        break;
+                }
+                preMenuStage++;
+                setUpGUI(2, getResources().getStringArray(R.array.activity_desc));
+                break;
+            case 2:
+                String activityString = spinner.getSelectedItem().toString();
+                switch (activityString) {
 
-    private int calcTargetProtein(int goal,  int targetKcal) {
-        int proteins;
-        if (goal == 0) {
-            proteins = targetKcal / 100 * 20;
-        } else if (goal == 1){
-            proteins = targetKcal / 100 * 30;
-        } else {
-            proteins = targetKcal/ 100 * 20;
+                    case "0-1":
+                        activityLevel = 1.2f;
+                        break;
+                    case "2-3":
+                        activityLevel = 1.375f;
+                        break;
+                    case "4-5":
+                        activityLevel = 1.55f;
+                        break;
+                    case "6-7":
+                        activityLevel = 1.725f;
+                        break;
+                    case "2 k./d.":
+                        activityLevel = 1.9f;
+                        break;
+                    default:
+                        break;
+                }
+                preMenuStage++;
+                setUpGUI(3, getResources().getString(R.string.age));
+                spinner.setVisibility(View.INVISIBLE);
+                inputText.setVisibility(View.VISIBLE);
+                break;
+            case 3:
+                inputString = inputText.getText().toString();
+                if (!inputString.isEmpty()) {
+                    age = Integer.parseInt(inputString);
+                    preMenuStage++;
+                    setUpGUI(4, getResources().getString(R.string.weight));
+                } else {
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.emptyField), Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 4:
+                inputString = inputText.getText().toString();
+                if (!inputString.isEmpty()) {
+                    weight = Float.parseFloat(inputText.getText().toString());
+                    preMenuStage++;
+                    setUpGUI(5, getResources().getString(R.string.height));
+                } else {
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.emptyField), Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 5:
+                inputString = inputText.getText().toString();
+                if (!inputString.isEmpty()) {
+                    height = Integer.parseInt(inputText.getText().toString());
+                    bmr = calcBmr(weight, height, age, gender);
+                    targetKcal = calcTargetKcal(bmr, activityLevel, difference);
+
+                         addUserData();
+
+
+                    // Moves to the next activity
+
+                        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                        startActivity(intent);
+                        finish();
+                } else {
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.emptyField), Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
         }
-
-        return proteins;
     }
 
-    private int calcTargetCarbs(int goal,  int targetKcal) {
-        int carbs;
-        if (goal == 0) {
-            carbs = targetKcal / 100 * 55;
-        } else if (goal == 1){
-            carbs = targetKcal / 100 * 45;
-        } else {
-            carbs = targetKcal/ 100 * 55;
+    public void moveBack(View view) {
+        if (preMenuStage != 0) {
+            switch (preMenuStage) {
+                case 1:
+                    preMenuStage--;
+                    setUpGUI(0, getResources().getStringArray(R.array.gender_list));
+                    buttonBack.setVisibility(View.INVISIBLE);
+                    break;
+                case 2:
+                    preMenuStage--;
+                    setUpGUI(1, getResources().getStringArray(R.array.main_goal));
+                    break;
+                case 3:
+                    preMenuStage--;
+                    setUpGUI(2, getResources().getStringArray(R.array.activity_desc));
+                    spinner.setVisibility(View.VISIBLE);
+                    inputText.setVisibility(View.INVISIBLE);
+                    break;
+                case 4:
+                    preMenuStage--;
+                    setUpGUI(3, getResources().getString(R.string.age));
+                    break;
+                case 5:
+                    preMenuStage--;
+                    setUpGUI(4, getResources().getString(R.string.height));
+                    break;
+                case 6:
+                    preMenuStage--;
+                    setUpGUI(5, getResources().getString(R.string.weight));
+                    break;
+                default:
+                    break;
+            }
         }
-
-        return carbs;
     }
 
-    private int calcTargetFat(int goal,  int targetKcal) {
-        int fat;
-        if (goal == 0) {
-            fat = targetKcal / 100 * 25;
-        } else if (goal == 1) {
-            fat = targetKcal / 100 * 25;
-        } else {
-            fat = targetKcal/ 100 * 25;
-        }
-
-        return fat;
-    }
 }
