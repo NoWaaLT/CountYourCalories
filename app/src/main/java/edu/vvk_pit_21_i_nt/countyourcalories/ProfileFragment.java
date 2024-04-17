597fcbf46f8e6337987e8b5e45a09af6d17a0bee
Audrius Ivko <audrius.ivko@gmail.com>
2024-04-17 20:39:21 +0300

    date find

--- app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/ProfileFragment.java
+++ app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/ProfileFragment.java
@@ -1,13 +1,15 @@
 package edu.vvk_pit_21_i_nt.countyourcalories;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +26,17 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private String myEmail;
+    private String myDisplayName;
+    private float myWeight;
+    private int myHeight;
+    private float myActivityLevel;
+    private int myAge;
+    private String myGender;
+    private float myBmr;
+    private int myGoal;
+    private int myTarget;
 
 
     public ProfileFragment() {
@@ -56,6 +69,10 @@ public class ProfileFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
 
         }
+        userDataRead();
+//        userDataPut();
+
+
         //Naudotojo duomenu keitimas
         //((MenuActivity) requireActivity()).updateUserData("age", 56);
         //Duomenu paemimas is MenuActivity naudotojo objekto
@@ -64,10 +81,73 @@ public class ProfileFragment extends Fragment {
         //Log.d("Age", String.valueOf(age));
     }
 
+    private void userDataPut(String key, String value) {
+        ((MenuActivity) requireActivity()).updateUserData(key, value);
+    }
+
+    private void userDataRead() {
+        myEmail = ((MenuActivity) requireActivity()).userDb.getEmail();
+        myDisplayName = ((MenuActivity) requireActivity()).userDb.getDisplayName();
+        myWeight = (int) ((MenuActivity) requireActivity()).userDb.getWeight();
+        myHeight = ((MenuActivity) requireActivity()).userDb.getHeight();
+        myActivityLevel = ((MenuActivity) requireActivity()).userDb.getActivityLevel();
+        myAge = ((MenuActivity) requireActivity()).userDb.getAge();
+        myGender = ((MenuActivity) requireActivity()).userDb.getGender();
+        myBmr = ((MenuActivity) requireActivity()).userDb.getBmr();
+        myGoal = ((MenuActivity) requireActivity()).userDb.getGoal();
+        myTarget = ((MenuActivity) requireActivity()).userDb.getTarget();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view;
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        TextView profile_Title = (TextView) view.findViewById(R.id.profile_title);
+        TextView age = (TextView) view.findViewById(R.id.profile_age);
+        TextView profile_Description = (TextView) view.findViewById(R.id.profile_description);
+
+
+//        TextView email =(TextView) view.findViewById(R.id.email);
+//        TextView displayName =(TextView) view.findViewById(R.id.displayName);
+//        TextView weight =(TextView) view.findViewById(R.id.weight);
+//        TextView height =(TextView) view.findViewById(R.id.height);
+//        TextView activityLevel =(TextView) view.findViewById(R.id.activityLevel);
+//        TextView gender = (TextView) view.findViewById(R.id.gender);
+//        TextView bmr =(TextView) view.findViewById(R.id.bmr);
+//        TextView goal =(TextView) view.findViewById(R.id.goal);
+//        TextView target =(TextView) view.findViewById(R.id.target);
+        profile_Title.setText(profile_Title());
+        profile_Description.setText(profile_description());
+        age.setText(myAge + " years old");
+
+        ImageView genderIconMale = (ImageView) view.findViewById(R.id.genderIconMale);
+        ImageView genderIconFemale = (ImageView) view.findViewById(R.id.genderIconFemale);
+
+
+        if (Objects.equals(myGender, "A Man")) {
+            genderIconMale.setVisibility(View.VISIBLE);
+            genderIconFemale.setVisibility(View.INVISIBLE);
+        } else {
+            genderIconMale.setVisibility(View.INVISIBLE);
+            genderIconFemale.setVisibility(View.VISIBLE);
+        }
+
+        return view;
+    }
+
+    private String profile_description() {
+        return "Hello, " + myDisplayName + "!\n" +
+                "You are " + myAge + " years old " + myGender + ".\n" +
+                "Your weight is " + myWeight + " kg "+ ".\n" +"and height is " + myHeight + " cm.\n" +
+                "Your activity level is " + myActivityLevel + ".\n" +
+                "Your BMR is " + myBmr + ".\n" +
+                "Your goal is " + myGoal + " and target is " + myTarget + ".";
+    }
+    private String profile_Title() {
+        return "My Profile";
     }
 }
