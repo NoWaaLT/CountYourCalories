c8ad93cb10d9d9e26668df9e5a72f4b19ee48722
TomasRay <tomasvaiciunas@live.com>
2024-04-21 19:24:12 +0300

    New nutrition page with working search feature.

---
+++ app/src/main/java/edu/vvk_pit_21_i_nt/countyourcalories/product.java
@@ -0,0 +1,59 @@
+package edu.vvk_pit_21_i_nt.countyourcalories;
+
+public class product {
+    float Angliavandeniai;
+    float Baltymai;
+    float Kilokalorijos;
+    String Produktas;
+    float Riebalai;
+
+    public product() {
+        Angliavandeniai = 0.01F;
+        Riebalai = 0.01F;
+        Baltymai = 0.01F;
+        Kilokalorijos = 0.01F;
+    }
+
+    public float getAngliavandeniai() {
+        return Angliavandeniai;
+    }
+
+    public void setAngliavandeniai(Float angliavandeniai) {
+        Angliavandeniai = angliavandeniai;
+    }
+
+    public float getBaltymai() {
+        return Baltymai;
+    }
+
+    public void setProduktas(String produktas) {
+        Produktas = produktas;
+    }
+
+    public void setBaltymai(Float baltymai) {
+        Baltymai = baltymai;
+    }
+
+    public float getKilokalorijos() {
+        return Kilokalorijos;
+    }
+
+    public void setKilokalorijos(Float kilokalorijos) {
+        Kilokalorijos = kilokalorijos;
+    }
+
+    public String getProduktas() {
+        return Produktas;
+    }
+
+    public float getRiebalai() {
+        return Riebalai;
+    }
+
+    public void setRiebalai(Float riebalai) {
+        Riebalai = riebalai;
+    }
+
+}
+
+
